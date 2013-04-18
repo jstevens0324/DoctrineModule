994fdd8b465c33dcae5a6e68dcc1b176c177f591
Josh Stevens <joshua.stevens@avimark.net>
2013-04-18 13:26:06 -0500

    First Commit

---
+++ config/module.config.php
@@ -0,0 +1,38 @@
+<?php
+return array(
+    'di' => array(
+        'definition' => array(
+            'class' => array(
+                'Memcache' => array(
+                    'addServer' => array(
+                        'host' => array('type' => false, 'required' => true),
+                        'port' => array('type' => false, 'required' => true),
+                    )
+                ),
+            ),
+        ),
+        'instance' => array(
+            'alias' => array(
+                // services
+                'doctrine_service' => 'DoctrineModule\Service\Service',
+                
+                // caching
+                'doctrine_memcache'       => 'Memcache',
+                'doctrine_cache_apc'      => 'Doctrine\Common\Cache\ApcCache',
+                'doctrine_cache_array'    => 'Doctrine\Common\Cache\ArrayCache',
+                'doctrine_cache_memcache' => 'Doctrine\Common\Cache\MemcacheCache',
+            ),
+            'doctrine_memcache' => array(
+                'parameters' => array(
+                    'host' => '127.0.0.1',
+                    'port' => '11211'
+                )
+            ),
+            'doctrine_cache_memcache' => array(
+                'parameters' => array(
+                    'memcache' => 'doctrine_memcache' 
+                )
+            ),
+        )
+    )
+);
---
+++ tests/SpiffyDoctrineTest/Doctrine/CliTest.php
@@ -0,0 +1,6 @@
+<?php
+namespace DoctrineModuleTest\Doctrine;
+
+class CliTest
+{
+}
